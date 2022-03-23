cb341d56f2a14441025ba3d2469ebc5a4912558e
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2022-03-23 22:16:24 +0900

    Welcome Message

--- app/src/main/java/com/example/mobile/MainActivity.java
+++ app/src/main/java/com/example/mobile/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView hide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hide = findViewById(R.id.hide_text);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hide.setText("앱 개발 팀에 오신 것을 환영합니다.");
+            }
+        });
     }
 }
